3afe3bbf237793826d9fa80d8dc0e18deae980f7
mashawesh <mashawesh@outlook.com>
2022-10-23 22:26:15 +0300

    add class

--- src/main/java/com/test/TestMaven.java
+++ src/main/java/com/test/TestMaven.java
@@ -3,6 +3,7 @@ package com.test;
 public class TestMaven {
 
     public void printString(){
-        System.out.println("Test the version");
+        System.out.println("Test the version 1234");
     }
+
 }
